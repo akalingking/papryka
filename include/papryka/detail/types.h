95f59e5007311f063c71cc41c11be36b183fbbb5
akalingking <akalingking@gmail.com>
2017-10-06 23:39:04 -0700

    support for c++17

--- include/papryka/detail/types.h
+++ include/papryka/detail/types.h
@@ -18,65 +18,13 @@
  */
 #pragma once
 #include <cinttypes>
-#include <decimal/decimal>
 #include "precision.h"
 #include <sstream>
 #include <cstring>
 #include <stdexcept>
 #include <iomanip>
 
-#if 0
-namespace { 
-    static const constexpr int MAX_DEC_LEN = 17;
-
-    inline std::decimal::decimal64 FromString(const char* str)
-    {
-        if (strlen(str) > MAX_DEC_LEN)
-            throw std::runtime_error("bad input");
-        char buf[MAX_DEC_LEN+1];
-        strncpy(buf, str, MAX_DEC_LEN+1);
-        char* point(NULL); 
-        if ((point = strchr(buf, '.')) != NULL)
-            *(point++) = '\0';
-        std::decimal::decimal64 ret(atoi(buf));
-        if (point != NULL && *point != '\0')
-        {
-            int exponent(strlen(point));
-            long long unsigned coeff(atoi(point));
-            std::decimal::decimal64 d2(std::decimal::make_decimal64(coeff, -exponent));
-            if (*buf == '-')
-                ret -= d2;
-            else
-                ret += d2;
-        }
-        return ret;    
-    }
-}
-
-// caveat: ostream does not support yet df
-inline std::ostream& operator<< (std::ostream& out, const std::decimal::decimal32& value) 
-{ 
-    double d(std::decimal::decimal_to_double(value));
-    return out << std::fixed << std::setprecision(papryka::precision::s_precision) << d;
-}
-inline std::ostream& operator<< (std::ostream& out, std::decimal::decimal64 const& value) 
-{ 
-    double d(std::decimal::decimal_to_double(value));
-    return out << std::fixed << std::setprecision(papryka::precision::s_precision) << d; 
-}
-inline std::ostream& operator<< (std::ostream& out, std::decimal::decimal128 const& value) 
-{ 
-    double d(std::decimal::decimal_to_double(value));
-    return out << std::fixed << std::setprecision(papryka::precision::s_precision) << d; 
-}
-#endif
-
 namespace papryka {
-    // @todo: use decimal types, do not use float or double in production
-    // @reference  <https://en.wikipedia.org/wiki/Decimal64_floating-point_format>
-    // at this point however decimal float is not yet entirely supported by stl (eg. floor, round, abs, ostream, etc.)
-    // take a look at precision for workaround using the double and float
-    //typedef std::decimal::decimal32 real_t;
     //typedef float real_t;
     typedef double real_t;
 }
